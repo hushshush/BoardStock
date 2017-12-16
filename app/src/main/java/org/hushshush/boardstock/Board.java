76a6b38c401f9ea6d0e580ec9394aa4b786b1b2f
Qisthia Kattania <q.kattania@gmail.com>
2017-12-16 14:47:48 +0700

    [QIS] Create UI for Restock Activity

--- app/src/main/java/org/hushshush/boardstock/Board.java
+++ app/src/main/java/org/hushshush/boardstock/Board.java
@@ -4,21 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Board extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_board);
 
-        LinearLayout unoRev3 = (LinearLayout) findViewById(R.id.uno_rev3);
-        unoRev3.setOnClickListener(new View.OnClickListener() {
+        TextView makeNew = (TextView) findViewById(R.id.make_new);
+        makeNew.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent unoRev3 = new Intent (MainActivity.this, Board.class);
-                startActivity(unoRev3);
+                Intent makeNew = new Intent (Board.this, Restock.class);
+                startActivity(makeNew);
             }
         });
     }
